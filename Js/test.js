5d1c80b24ce3af797c78849c5321a16093f203d7
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2023-11-13 19:14:46 +0330

    add some data

---
+++ Js/test.js
@@ -0,0 +1,63 @@
+/// variable
+///music player
+///catgory
+const songs = [
+    {
+      path:
+        "music/rain.mp3",
+      displayName: "Html Padcast",
+      artist: "Ozbi",
+      cover:
+        "https://images.genius.com/ee202c6f724ffd4cf61bd01a205eeb47.1000x1000x1.jpg",
+    },
+    {
+      path: "music/Reza Pishro - Bache Bahala (320).mp3",
+      displayName: "Bache Bahala",
+      artist: "Reza Pishro",
+      cover: "images/peakpx.jpg",
+    },
+    {
+      path:
+        "https://v.delgarm.com/mp3/828/2021/01/12/82089ea02b9842c4178b03c829247070.mp3",
+      displayName: "rain",
+      artist: "rain and tunder",
+   
+    },
+    {
+    path:
+    "https://dl2.hitseda.com/Music/bm/021/khareji/rock/1/01.%20-%20Another%20World_766.mp3",
+  displayName: "Another World",
+  artist: "IDK?",
+    
+},{
+path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/Aaron%20-%20U-Turn%20%28Lili%29.mp3",
+displayName: "U-Turn( liliy)",
+artist: "Aoran",
+
+},
+{ path:
+  "https://dl2.hitseda.com/Music/ak/nab-selections/CHRIS%20DE%20BURGH%20-The%20Last%20Time%20I%20Cried.mp3",
+  displayName: "The Last Time I Cried",
+  artist: "CHRIS DE BURGH",},
+  { path:
+    "https://dl2.hitseda.com/Music/ak/nab-selections/Flori%20Mumajesi%20-%20Ku%20isha%20une.mp3",
+    displayName: "Ku isha une",
+    artist: "Flori Mumajesi & Argjentina",},
+  ];
+let RGBColor=[
+    "#f5636399",
+    "#f5946399",
+    "#f5bd6399",
+    "eef56399",
+    "#9df563bb",
+    "#63f585bb",
+    "#63f5c0bb",
+    "#63e6f5bb",
+    "#63acf5bb",
+    "#6379f5bb",
+    "#8863f5bb",
+    "#c763f5bb",
+    "#f563e9bb",
+    "#f5636abb"
+]
