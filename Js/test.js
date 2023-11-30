0f724b97964690bda0c661a26b4fbdeb2277343d
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2023-11-30 15:17:14 +0330

    set prescrollbar and song function

--- Js/test.js
+++ Js/test.js
@@ -27,13 +27,15 @@ if(targetLabel.id=="removelabel"){removeLabel.innerHTML="";removeLabel.classList
 function checkScreenWidth(){
   if(window.screen.width>680){
     addLabel.addEventListener("mouseenter",setLabelIcon)
-removeLabel.addEventListener("mouseenter",setLabelIcon)
-addLabel.addEventListener("mouseleave",setLabelText)
-removeLabel.addEventListener("mouseleave",setLabelText)
+    removeLabel.addEventListener("mouseenter",setLabelIcon)
+    addLabel.addEventListener("mouseleave",setLabelText)
+    removeLabel.addEventListener("mouseleave",setLabelText)
   }else{
     console.log(window.screen.width);
     
   }}
+  window.addEventListener("load",checkScreenWidth)
+//window.addEventListener("change",checkScreenWidth)
 // create and add new category
 function newCatgory(){
   let catgoryname=prompt("please enter the name of the catgory:",)
@@ -187,11 +189,255 @@ if(index>RGBColor.length){
 }
 }
 
-window.addEventListener("change",checkScreenWidth)
+
 
 ////////////////////play and load song (variable ,data and function)
 // code will add in the futur
+let musiclist=document.getElementById("musiclist")
+const songs = [
+  {
+    path:
+      "music/rain.mp3",
+    displayName: "Html Padcast",
+    artist: "Ozbi",
+    id:1,
+    cover:
+      "https://images.genius.com/ee202c6f724ffd4cf61bd01a205eeb47.1000x1000x1.jpg",
+  },
+  {
+    path: "music/Reza Pishro - Bache Bahala (320).mp3",
+    displayName: "Bache Bahala",
+    artist: "Reza Pishro",
+    id:2,
+    cover: "images/peakpx.jpg",
+  },
+  {
+    path:
+      "https://v.delgarm.com/mp3/828/2021/01/12/82089ea02b9842c4178b03c829247070.mp3",
+    displayName: "rain",
+    artist: "rain and tunder",
+    id:3,
+ 
+  },
+  {
+  path:
+  "https://dl2.hitseda.com/Music/bm/021/khareji/rock/1/01.%20-%20Another%20World_766.mp3",
+displayName: "Another World",
+artist: "IDK?",
+id:4,
+  
+},{
+path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/Aaron%20-%20U-Turn%20%28Lili%29.mp3",
+displayName: "U-Turn( liliy)",
+artist: "Aoran",
+id:5,
 
+},
+{ path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/CHRIS%20DE%20BURGH%20-The%20Last%20Time%20I%20Cried.mp3",
+displayName: "The Last Time I Cried",
+artist: "CHRIS DE BURGH",
+id:6},
+{ path:
+  "https://dl2.hitseda.com/Music/ak/nab-selections/Flori%20Mumajesi%20-%20Ku%20isha%20une.mp3",
+  displayName: "Ku isha une",
+  artist: "Flori Mumajesi & Argjentina",
+  id:7}, 
+ {path:
+"https://v.delgarm.com/mp3/828/2021/01/12/82089ea02b9842c4178b03c829247070.mp3",
+displayName: "rain",
+artist: "rain and tunder",
+id:8},
+{
+path:
+"https://dl2.hitseda.com/Music/bm/021/khareji/rock/1/01.%20-%20Another%20World_766.mp3",
+displayName: "Another World",
+artist: "IDK?",
+id:9},{
+path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/Aaron%20-%20U-Turn%20%28Lili%29.mp3",
+displayName: "U-Turn( liliy)",
+artist: "Aoran",
+id:10},
+{ path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/CHRIS%20DE%20BURGH%20-The%20Last%20Time%20I%20Cried.mp3",
+displayName: "The Last Time I Cried",
+artist: "CHRIS DE BURGH",
+id:11},
+
+{ path:
+"https://dl2.hitseda.com/Music/ak/nab-selections/Flori%20Mumajesi%20-%20Ku%20isha%20une.mp3",
+displayName: "Ku isha une",
+artist: "Flori Mumajesi & Argjentina",
+id:12}
+ 
+];
+const user=(name,artist,time)=>{
+this.songname=name
+this.artist=artist
+//this.time=time
+
+}
+function limaker(name,artist,time){
+
+let li=document.createElement("li")
+
+}
+songs.forEach(object=>{
+
+  musiclist.insertAdjacentHTML("beforeend",` <li id=${object.id} class="mt-1 w-full h-10 bg-orange-400  grid grid-cols-2 px-2 rounded-xl hover:scale-105 hover:-py-1">
+            
+  <div class="flex items-center">00:00</div>
+  <div class="px-1">
+     <h1 class=" font-serif text-sm overflow-hidden whitespace-nowrap">${object.displayName}</h1>
+     <h4 class="float-right font-serif text-xs overflow-hidden whitespace-nowrap">${object.artist}</h4>
+  </div>
+  </li>`)
+}
+
+)
+
+
+
+
+
+
+
+
+
+
+    // ProgressBar
+    function updateProgressBar(e) {
+      if (isPlaying) {
+        const duration = e.srcElement.duration;
+        const currentTime = e.srcElement.currentTime;
+        // 
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = progressPercent + "%";
+        // 
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+          durationSeconds = "0" + durationSeconds;
+        }
+        //
+        if (durationSeconds) {
+          durationEl.textContent = durationMinutes + ":" + durationSeconds;
+        }
+        // 
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+          currentSeconds = "0" + currentSeconds;
+        }
+        currentTimeEl.textContent = currentMinutes + ":" + currentSeconds;
+      }
+    }
+ //progress
+
+const currentTimeEl = document.getElementById("current-time");
+
+const durationEl = document.getElementById("duration");
+
+const progressContainer = document.getElementById("progress-container");
+
+const progress = document.getElementById("progress");
+
+const prevBtn = document.getElementById("prev");
+
+const playBtn = document.getElementById("play");
+
+const nextBtn = document.getElementById("next");
+    
+let audioBtn=document.getElementById("play")
+let audio=document.querySelector("audio")
+let isPlaying=false
+
+let songIndex=0;
+///// function
+// Previous Song
+function loadSong(song) {
+
+    audio.src = song.path;
+    console.log(song.id);
+    let liadd=document.getElementById(song.id)
+    console.log(liadd);
+    liadd.classList.add("bg-opacity-40")
+if (song.id>1) {
+  let liremove=document.getElementById(song.id-1)
+  liremove.classList.remove("bg-opacity-40")
+}else{
+  let liremove=document.getElementById(12)
+  liremove.classList.remove("bg-opacity-40")
+}
+  }
+  ///song information dispaly
+  function songInformation(song){
+
+  }
+
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    songInformation(songs[songIndex])
+    play();
+  }
+  
+  // Next Song
+  function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    songInformation(songs[songIndex])
+    play();
+  }
+function play(){
+    isPlaying=true
+    audio.play()
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+}
+function pause(){
+    isPlaying=false
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+
+    audio.pause()
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+    // Set ProgressBar
+    function setProgressBar(e) {
+      const width = this.clientWidth;
+      const clickX = e.offsetX;
+      const duration = audio.duration;
+      audio.currentTime = (clickX / width) * duration;
+    }
+
+    prevBtn.addEventListener("click", prevSong);
+    nextBtn.addEventListener("click", nextSong);
+    audio.addEventListener("timeupdate", updateProgressBar);
+    progressContainer.addEventListener("click", setProgressBar);
+
+    progress.style.backgroundColor="cadetblue"
+
+
+    audioBtn.addEventListener("click",function(){
+ 
+      if(isPlaying){
+          pause()
+      }else{
+          play()
+      }
+      console.log(audioBtn.pause)
+      })
 
 ////////////////////calender and reminder part
 //code will add in the future
