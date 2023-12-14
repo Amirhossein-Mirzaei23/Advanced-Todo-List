aaca2893071e802de21fe9b4e39d606792179ac9
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2023-12-14 11:22:05 +0330

    Update test.js

--- Js/test.js
+++ Js/test.js
@@ -205,7 +205,7 @@ if(index>RGBColor.length){
 
 
 //////////////////////////////////////////play and load song (variable ,data and function)
-//////////////////////////////////////////// 
+//////////////////////////////////////////// music playlist
 let musiclist=document.getElementById("musiclist")
 const songs = [
   {
@@ -286,6 +286,44 @@ artist: "Flori Mumajesi & Argjentina",
 id:12}
  
 ];
+let house =[
+{ path:"https://dl2.hitseda.com/Music/ak/nab-selections/Otnicka%20-%20Mandoline.mp3",
+displayName: "mandoline",
+artist: "otnicka",
+time:"03:20",
+id:1
+},
+{ path:"https://dl2.hitseda.com/Music/ak/nab-selections/Ghostly%20Kisses%20-%20Roses.mp3",
+displayName: "Roses",
+artist: " Ghostly Kisses",
+time:"03:28",
+id:2
+},
+{ path:"https://dl2.hitseda.com/Music/ak/insta-music/Jah%20Khalib%20-%20Leyla.mp3",
+displayName: "Leylal",
+artist: "Jah Khalìb",
+time:"04:06",
+id:3
+},
+{ path:"https://dl2.hitseda.com/Music/ak/insta-music/JONY-Love-your-voice.mp3",
+displayName: "Love your voice",
+artist: "JOHNY",
+time:"02:30",
+id:4
+},
+{ path:"",
+displayName: "",
+artist: "",
+time:"3:20",
+id:5
+},
+{ path:"",
+displayName: "",
+artist: "",
+time:"3:20",
+id:6
+},
+]
 const user=(name,artist,time)=>{
 this.songname=name
 this.artist=artist
