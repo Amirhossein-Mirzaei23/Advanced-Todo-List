7cfb5266aae81d60f4704422ab06d70943bd4a95
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2023-12-17 22:37:03 +0330

    Update test.js

--- Js/test.js
+++ Js/test.js
@@ -1,9 +1,9 @@
 window.addEventListener("load",()=>{
  // alert("for add a new note to every category plz select your category at the first step").toUpperCase()
  // alert("by single Double click on each item ,item will be deleted")
-//  alert(`Hi ,this is an under development Todolist project which add a few feather such as music palyer and calender part to be more user friendly and practical`)
-//  alert(`calendar and reminder part is incomplete yet, many feather such as tutorial ,reminder,daily motivaion,dragable remove will add as soon as posible`)
-//  alert(`And Welcome 🥳`)
+  alert(`Hi ,this is an under development Todolist project which add a few feather such as music palyer and calender part to be more user friendly and practical`)
+  alert(`calendar and reminder part is incomplete yet, many feather such as tutorial ,reminder,daily motivaion,dragable remove will add as soon as posible`)
+  alert(`And Welcome 🥳`)
 })
 ////////////////////  Add and make Catgory part (variable and function)
 const body=document.querySelector("body")
